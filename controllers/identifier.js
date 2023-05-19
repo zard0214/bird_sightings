10c9e61afda357d1fdf1a04b945fd1849b4aff17
i-akshat-jain <108737870+akshatdebtenforce@users.noreply.github.com>
2023-05-19 14:24:07 +0100

    knowledge graph error done

--- controllers/identifier.js
+++ controllers/identifier.js
@@ -4,25 +4,14 @@ const endpointUrl = 'http://dbpedia.org/sparql';
 const fetch = require('node-fetch');
 // Define function to search for bird species and create new bird sighting document
 
-async function fetchBirdURI(location, description) {
-    const [latitude, longitude] = location.split(",");
+async function fetchBirdURI(identification) {
+
     const sparqlQuery = `
-PREFIX dbo: <http://dbpedia.org/ontology/>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
-SELECT ?name ?description ?dbpediaLink
-WHERE {
-  ?bird a dbo:Bird ;
-        rdfs:label ?name ;
-        dbo:abstract ?description ;
-        foaf:depiction ?image ;
-        dbo:wikiPageExternalLink ?dbpediaLink .
-  FILTER(regex(?name, "${description}", "i"))
-  FILTER(langMatches(lang(?description), "en"))
-  FILTER(?lat - ${latitude} < 0.01 && ${latitude} - ?lat < 0.01)
-  FILTER(?long - ${longitude} < 0.01 && ${longitude} - ?long < 0.01)
-}
-LIMIT 1
+    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+    SELECT ?link
+    WHERE {
+    <http://dbpedia.org/resource/${identification}> foaf:isPrimaryTopicOf ?link.
+    }
   `;
 
     const encodedQuery = encodeURIComponent(sparqlQuery);
